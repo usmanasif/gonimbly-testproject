29d16cf9b268629f1a8d8cd683252b054c1e7474
Usman Asif <usmanasif@users.noreply.github.com>
2019-07-10 16:27:59 +0500

    initial commit

---
+++ src/components/user_form.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./styles/style.css";
+
+const IMAGE_HOST = "https://robohash.org/";
+
+class UserForm extends Component {
+  state = {
+    current_image_url: "",
+    showImage: false
+  };
+
+  handleSearch = event => {
+    this.setState({ current_image_url: event.target.value, showImage: false });
+  };
+
+  handleSubmit = _ => {
+    this.setState({
+      current_image_url: IMAGE_HOST.concat(this.state.current_image_url),
+      showImage: true
+    });
+  };
+
+  render() {
+    const { current_image_url, showImage } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <h1 className="col-sm display-4 mt-5">Usman Asif</h1>
+        </div>
+
+        <div className="row row-alignment mt-5 ml-0">
+          <input
+            type="text"
+            className="form-control col-md-3 col-10"
+            placeholder="Enter anything..."
+            onChange={this.handleSearch}
+          />
+          <button
+            className="btn btn-primary btn-sm btn-width col-md-1 col-10"
+            type="button"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+
+        <div className="row mt-4 row-alignment">
+          {showImage && (
+            <img
+              src={current_image_url}
+              className="img-fluid magictime spaceInRight"
+              alt="robohash img"
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserForm;
